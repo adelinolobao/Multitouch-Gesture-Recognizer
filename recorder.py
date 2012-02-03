a7164ba5bcfe8e0087f966e8178b7c2394442a99
Adelino Lobão <sousalobao@gmail.com>
2012-02-03 18:33:01 +0000

    added recorder file

---
+++ recorder.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+File: recorder.py
+Adelino Lobão
+03/02/2012
+"""
+
+import sys
+import os
+import pickle
+from pymt import *
+from lib.window import WindowApp
+from lib.helper import Helper
+
+class RecorderWindowApp(WindowApp):
+	def on_touch_up(self, touch):
+		self.gesture.append(touch.userdata['trace'])
+		if len(getCurrentTouches()) == 0:
+			templatePath = Helper.returnTemplatePath()
+			output = open(templatePath, 'wb')
+			pickle.dump(self.gesture, output)
+			output.close()
+			print templatePath + ' saved...'
+			self.gesture = []
+
+if __name__ == '__main__':
+	window = MTWindow()
+	app = RecorderWindowApp()
+	window.add_widget(app)
+	runTouchApp()

9fea20e8d5b5760f40f54175654847ef052b0410
Adelino Lobão <sousalobao@gmail.com>
2012-02-06 16:49:58 +0000

    added recognition function

--- lib/recognizer.py
+++ lib/recognizer.py
@@ -6,7 +6,16 @@ Adelino Lobão
 04/02/2012
 """
 
+import math
 from template import Template
+from helper import Helper
+
+"""
+Constants
+"""
+halfDiagonal   = 0.5 * math.sqrt(250.0 * 250.0 + 250.0 * 250.0)
+angleRange     = 45.0
+anglePrecision = 2.0
 
 """
 Class Recognizer
@@ -14,6 +23,30 @@ Class Recognizer
 class Recognizer:
 	def __init__(self):
 		self.templates = list()
+		
+	#recognize the gesture
+	def recognize(self, traces):
+		#security verification
+		for trace in traces:
+			if len(trace) < 2:
+				return (None, 0)	#return none gesture
+		#information about the gesture	
+		numTracesGesture = len(traces)
+		pointsGesture = Helper.normalizeGesture(traces)
+		#comparison process
+		bestDistance = float('infinity')
+		bestTemplate = None
+		for template in self.templates:
+			distance = Helper.distanceAtBestAngle(pointsGesture, numTracesGesture, template, -angleRange, +angleRange, anglePrecision)
+			if distance < bestDistance:
+				bestDistance = distance
+				bestTemplate = template
+				score = 1.0 - (bestDistance / halfDiagonal)
+		#get the best template
+		if bestTemplate == None:
+			return(None, 0)
+		else:
+			return (bestTemplate.name, score)
 		
 	#add a template
 	def addTemplate(self, name, traces):
